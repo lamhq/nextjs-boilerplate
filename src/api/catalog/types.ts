9f83f44e4996bbeef04bf3ba40f414a02a2171fa
Lam <daibanglam@gmail.com>
2023-09-13 10:23:20 +0700

    feat: implement product page with no css

---
+++ src/api/catalog/types.ts
@@ -0,0 +1,7 @@
+export interface Product {
+  name: string;
+  slug: string;
+  images: string[];
+  price: number;
+  description: string;
+}
--- src/catalog/product/pages/ProductDetailPage/index.tsx
+++ src/catalog/product/pages/ProductDetailPage/index.tsx
@@ -1,3 +1,8 @@
-export default function ProductDetailPage() {
-  return <p>product</p>;
+import getService from '@/common/utils/getService';
+import ProductService from '../../services/ProductService';
+
+export default async function ProductDetailPage({ params }: { params: { slug: string } }) {
+  const productService = getService(ProductService);
+  const product = await productService.findOneBySlug(params.slug);
+  return <>{product.name}</>;
 }
